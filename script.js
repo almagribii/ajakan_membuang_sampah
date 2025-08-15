e96ad2aab90396700019596070ae8b716783cdc5
Almagribii <brucadalm@gmail.com>
2025-08-15 21:00:23 +0700

    add javascript

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document
+$(document).ready(function() {
+
+    // Efek smooth scrolling saat tombol diklik
+    $("#cta-button").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("#commit-button").offset().top
+        }, 1000); // Waktu animasi 1000ms
+    });
+
+    // Feedback saat tombol "Saya Berkomitmen" diklik
+    $("#commit-button").on("click", function() {
+        alert("Terima kasih atas komitmen Anda! Mari kita jaga bumi bersama.");
+    });
+});
